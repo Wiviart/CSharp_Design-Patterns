df24c1f1df969c8d92cf49a6ba844fe4310071ae
Wiviart <dungphamhuu95@gmail.com>
2024-02-07 22:29:14 +0700

    First Commit

---
+++ Other Patterns/Game Loop/Program.cs
@@ -0,0 +1,35 @@
+﻿public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        GameManager gameManager = new GameManager();
+        await gameManager.Run();
+    }
+}
+
+class GameManager
+{
+    int frameCount = 0;
+    public async Task Run()
+    {
+        Initialize();
+
+        while (true)
+        {
+            Render();
+            await Task.Delay(1000 / 60);
+            frameCount++;
+        }
+    }
+
+    private void Initialize()
+    {
+        Console.WriteLine("Initializing game...");
+    }
+
+    private void Render()
+    {
+        DateTime time = DateTime.Now;
+        Console.WriteLine($"[{time}] Rendering frame No.{frameCount}");
+    }
+}
