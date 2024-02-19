2623735dbcd760d9d98fb67ce0a996e9bca5e6ec
Wiviart <dungphamhuu95@gmail.com>
2024-02-19 14:16:36 +0700

    Update

---
+++ Creational Patterns/Object/Abstract Factory/Core/Room.cs
@@ -0,0 +1,25 @@
+class Room : MapSite
+{
+    public Room(int roomNumber)
+    {
+        _roomNumber = roomNumber;
+    }
+    public MapSite GetSide(Direction direction)
+    {
+        return _sides[(int)direction];
+    }
+    public void SetSide(Direction direction, MapSite mapSite)
+    {
+        _sides[(int)direction] = mapSite;
+    }
+    public override void Enter()
+    {
+        for (int i = 0; i < _sides.Length; i++)
+        {
+            _sides[i].Enter();
+        }
+    }
+    private MapSite[] _sides = new MapSite[4];
+    private int _roomNumber;
+    public int RoomNumber { get { return _roomNumber; } }
+}
