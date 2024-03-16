0a93b1ed68d56bafcec3b942feb491f2e6993a37
Wiviart <dungphamhuu95@gmail.com>
2024-03-16 17:59:00 +0700

    Update

--- Creational Patterns/Object/Prototype/Prototype 1/Program.cs
+++ Creational Patterns/Object/Prototype/Prototype 1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Person person = new Person(20, new DateTime(1990, 1, 1), "John", new IdInfo(123456));
+person.Display();
+
+Person shallowCopy = person.ShallowCopy();
+shallowCopy.Display();
+
+Person deepCopy = person.DeepCopy();
+deepCopy.Display();
+
+Person clone = person.Clone();
+clone.Display();
+
+Console.WriteLine("\nAfter changes:");
+
+person.Age = 30;
+person.BirthDate = new DateTime(1995, 1, 1);
+person.Name = "Tom";
+person.IdInfo.IdNumber = 654321;
+
+person.Display();
+shallowCopy.Display();
+deepCopy.Display();
+clone.Display();
--- Creational Patterns/Object/Prototype/Prototype 2/Program.cs
+++ Creational Patterns/Object/Prototype/Prototype 2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿MazeGame mazeGame = new MazeGame();
+
+Maze maze = new Maze();
+Room room = new Room();
+Door door = new Door();
+Wall wall = new Wall();
+
+Maze aMaze = mazeGame.CreateMaze(maze, room, door, wall);
+aMaze.Enter();
